2442273e4296d39782085b999e306a62a4376716
Jemima <21041289@itdurango.edu.mx>
2024-05-22 21:02:18 -0600

    Initial commit

---
+++ app/src/main/java/com/example/listas1905/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.listas1905;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    String[] comida = {
+            "Hamburguesas",
+            "Pizza",
+            "Gelatina",
+            "Ceviche",
+            "Albóndigas",
+            "Spaguetti",
+            "Ravioli",
+            "Tacos",
+            "Chilaquiles",
+            "Molletes"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, comida));
+    }
+
+    public void onListItemClick(ListView padre, View v, int posicion, long id){
+        Toast.makeText(this, "Seleccionó "+comida[posicion], Toast.LENGTH_LONG).show();
+
+    }
+}
